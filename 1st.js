4d59f6914da00e5ff41e8628f1435f61617ec33b
WanHa <1043346131@qq.com>
2017-09-22 13:31:25 +0800

    nodejs 學習 第一天

---
+++ 1st.js
@@ -0,0 +1,6 @@
+'user strict'
+console.log('hello world!');
+
+var user='world';
+var s=`Hello ${user}`;
+console.log(s);
--- test.js
+++ test.js
@@ -8,6 +8,21 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+ 
+    var uuid = s.join("");
+
+
+
+
 //查询
 connection.query('select * from test.t2', function(err, rows, fields) {
     if (err) throw err;
@@ -18,9 +33,3 @@ connection.query('select * from test.t2', function(err, rows, fields) {
 //关闭连接
 connection.end();
 
-function guid() {
-        function S4() {
-           return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-        }
-        return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-    }
